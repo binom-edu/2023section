98e8507b1901f4f2b7b927bb7d649bd4572bbeaa
klindyuk <klindyuk@gmail.com>
2024-11-05 17:02:20 +0300

    29 октября

--- 2024-25/battlecity/game.py
+++ 2024-25/battlecity/game.py
@@ -10,15 +10,32 @@ class Tank(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.x, self.y = coords
         self.rect.topleft = (self.x * cfg['TILESIZE'], self.y * cfg['TILESIZE'])
-        self.last_update = pygame.time.get_ticks()
+        self.last_update_c = self.last_update = pygame.time.get_ticks()
         self.frame_rate = 100
+        self.speed = 1
+        self.direction = 0
+        self.block_move = False
         all_sprites.add(self)
     
     def update(self):
         now = pygame.time.get_ticks()
-        if now - self.last_update > self.frame_rate:
+        if now - self.last_update_c > self.speed:
+            self.block_move = True
+            if self.rect.top > self.y * cfg['TILESIZE']:
+                self.rect.top -= 1
+            elif self.rect.top < self.y * cfg['TILESIZE']:
+                self.rect.top += 1
+            elif self.rect.left < self.x * cfg['TILESIZE']:
+                self.rect.left += 1
+            elif self.rect.left > self.x * cfg['TILESIZE']:
+                self.rect.left -= 1
+            else:
+                self.block_move = False
+            self.last_update_c = now
+
+        if now - self.last_update > self.frame_rate:    
             self.img_num = (self.img_num + 1) % len(self.imgs)
-            self.image = self.imgs[self.img_num]
+            self.image = pygame.transform.rotate(self.imgs[self.img_num], self.direction * 90)
             self.last_update = now
 
 class Player(Tank):
@@ -31,7 +48,22 @@ class Player(Tank):
         self.image = self.imgs[0]
         self.img_num = 0
 
-
+    def update(self):
+        keystate = pygame.key.get_pressed()
+        if not self.block_move:
+            if keystate[pygame.K_UP]:
+                self.direction = 0
+                self.y = max(self.y - 1, 0)
+            elif keystate[pygame.K_LEFT]:
+                self.direction = 1
+                self.x = max(self.x - 1, 0)
+            elif keystate[pygame.K_DOWN]:
+                self.direction = 2
+                self.y = min(self.y + 1, cfg['HEIGHT'] - 2)
+            elif keystate[pygame.K_RIGHT]:
+                self.direction = 3
+                self.x = min(self.x + 1, cfg['WIDTH'] - 2)
+        Tank.update(self)
 
 
 def readcfg():
@@ -64,6 +96,7 @@ while gameOn:
     # обновления
     all_sprites.update()
     # рендеринг
+    screen.fill((0, 0, 0))
     all_sprites.draw(screen)
 
     pygame.display.flip()
